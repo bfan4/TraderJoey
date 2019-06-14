3111c2b93b9b09cb2559b5feb496e78a2cc6bd91
mchellor <mc.hellor@gmail.com>
2019-06-14 00:42:18 -0400

    20190614

---
+++ Trading/src/main/java/com/traderjoey/entity/Currency.java
@@ -0,0 +1,47 @@
+package com.traderjoey.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="currency")
+public class Currency {
+	
+	
+	@Id
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="current_price")
+	private String currentPrice;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrice() {
+		return currentPrice;
+	}
+
+	public void setPrice(String price) {
+		this.currentPrice = price;
+	}
+
+}
